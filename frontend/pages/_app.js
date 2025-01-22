cfb0befbd9c27aa4e22eb443e7db80d19fd145f2
toni798 <anthonysu798.ca@gmail.com>
2025-01-22 01:22:53 -0500

    Modified _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  );
 }
