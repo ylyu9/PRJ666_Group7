2cdf135b3e3c4f855ce0aae1eac992631f4041a5
toni798 <anthonysu798.ca@gmail.com>
2025-01-22 01:32:06 -0500

    Added More route for the frontend usage

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,16 +1,20 @@
 import express from "express";
 
 // Import the controllers from the controllers folder 
-import { signup, login, logout, googleLogin } from "../controllers/auth.controllers.js";
+import { signup, login, logout, googleAuth, requestPasswordReset, resetPassword } from "../controllers/auth.controllers.js";
 const router = express.Router();
 
 // Defind the routes for signup, login, and logout
-router.get("/signup", signup);
-router.get("/login", login);
-router.get("/logout", logout);
+router.post("/signup", signup); // localhost:4000/api/auth/signup
+router.post("/login", login); // localhost:4000/api/auth/login
+router.get("/logout", logout); // localhost:4000/api/auth/logout
 
 // Google login route
-router.post("/google", googleLogin);
+router.post("/googleAuth", googleAuth); // localhost:4000/api/auth/googleAuth
+
+// Password reset routes
+router.post("/requestPasswordReset", requestPasswordReset); // localhost:4000/api/auth/requestPasswordReset
+router.post("/resetPassword", resetPassword); // localhost:4000/api/auth/resetPassword
 
 // Export the router
 export default router;
