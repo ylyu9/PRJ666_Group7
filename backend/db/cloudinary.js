fe78ec998f30c5f1f88ffba4a8681ee86e65b39c
toni798 <anthonysu798.ca@gmail.com>
2025-01-22 01:28:20 -0500

    Added cloudinary for story the user profile image

---
+++ backend/db/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'profile_images',
+        allowed_formats: ['jpeg', 'png', 'jpg'],
+    },
+});
+
+console.log('Cloudinary Config:', {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET ? '******' : 'Not Set', // Mask secret
+});
+
+export default storage;
