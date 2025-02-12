e44fa0cda6caaa4684c3bfe4d8b456d3565b0592
toni798 <anthonysu798.ca@gmail.com>
2025-02-11 20:06:21 -0500

    Added protection for all the get user information routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
 
 // Import the controllers from the controllers folder 
 import { signup, login, logout, googleAuth, requestPasswordReset, resetPassword } from "../controllers/auth.controllers.js";
@@ -7,7 +8,7 @@ const router = express.Router();
 // Defind the routes for signup, login, and logout
 router.post("/signup", signup); // localhost:4000/api/auth/signup
 router.post("/login", login); // localhost:4000/api/auth/login
-router.get("/logout", logout); // localhost:4000/api/auth/logout
+router.get("/logout", protect, logout); // localhost:4000/api/auth/logout
 
 // Google login route
 router.post("/googleAuth", googleAuth); // localhost:4000/api/auth/googleAuth
